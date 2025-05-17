d963bf80063b6a6376910f23f851ad7aa4b87fef
Dr0wsyNess <145124455+Dr0wsyNess@users.noreply.github.com>
2025-05-17 17:49:58 +1000

    [SEARCHBAR NOT WORKING YET] continue to add to index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-// $(document).ready(function(){
+$(document).ready(function () {
+    var carTags = [];
+    $.ajax({
+        url: "cars.json",
+        type: "get",
+        data: { term: term },
+        dataType: "JSON",
+        success: function (response) {
+            for (var i = 0; i < response.length; i++) {
+                carTags.push(response[i].carType);
+                carTags.push(response[i].brand);
+                carTags.push(response[i].carModel);
+                carTags.push(response[i].description);
+            }
+        }
+    });
 
-// })
+    //use the id tag to autocomplete the search bar from the suggestions in carTags
+    $("#searchBar").autocomplete({
+        source: availableTags,
+    });
 
-var carTags = [];
-$.ajax({
-    url: "cars.json",
-    type: "get",
-    dataType: "JSON",
-    success: function (response) {
+    $("#searchBar").on("keyup", function () {
+        var searchedValue = $(this).val().toLowerCase();
 
-    }
-});
-
-$("#searchBar").autocomplete({
-    source: availableTags,
-});
+    })
+})
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="index" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <title>Online Car Rental System</title>
 </head>
 
@@ -55,7 +55,7 @@
         <br>University of Technology, Sydney
     </p>
 </footer>
-
+<script src="index.js"></script>
 </html>
 
 <?php
